0e533a96fb944fd34db48f3cf344e7096c179a6d
Lei Chen <leichen88@yahoo.fr>
2022-02-14 21:39:25 +0100

    add custom header and footer template

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+// Header template
+class hcrHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header class="p-3 border-bottom">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/img/unhcr_logo_blue.svg" alt="UNHCR Logo"/>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Chart gallery</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">General guidance</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tools</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li>
+                                        <a class="dropdown-item" href="#">R</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">Adobe Illustrator</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">MS PowerBI</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">MS Excel</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link">Products</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    `
+    }
+}
+customElements.define ('hcr-header', hcrHeader)
+
+
+// Footer template
+class hcrFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="bg-dark p-3 mt-auto">
+            <div class="container">
+                <div class="d-flex flex-wrap justify-content-between align-items-center mb-2">
+                    <div class="navbar navbar-expand navbar-dark">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Chart gallery</a></li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">General guidance</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Tools</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Products</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <button type="button" class="btn btn-primary justify-content-end">
+                      Contact us
+                    </button>
+                </div>
+                <p class="text-muted"><small>© UNHCR 2022</small></p>
+            </div>
+        </footer>
+    `
+    }
+}
+customElements.define ('hcr-footer', hcrFooter)
