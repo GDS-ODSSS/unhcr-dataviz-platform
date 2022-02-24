695eb1040264ba1fd12b581b750061d6f5defd7b
Lei Chen <leichen88@yahoo.fr>
2022-02-24 15:43:31 +0100

    fix chart page with sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,20 @@ $(window).on('load', function() {
   var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#sidebar'
   })
+
+
+  //Back to top
+  var btn = $('#button');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+  
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
