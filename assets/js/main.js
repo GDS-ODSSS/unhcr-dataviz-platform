6fccf0239f0392f49221fb13ff0929d8a7938fa9
Lei Chen <leichen88@yahoo.fr>
2022-02-25 16:37:48 +0100

    update css

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,34 +104,30 @@ $(window).on('load', function() {
   });
 
 
-  //Sidebar scrolling
-  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#sidebar'
-  })
+//Sidebar scrolling
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#sidebar'
+})
+//Back to top
+var btn = $('#button');
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
 
-
-  //Back to top
-  var btn = $('#button');
-
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
-    } else {
-      btn.removeClass('show');
-    }
-  });
-  
-  btn.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
-  });
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
 
 
 // Product catalogue isotope and filter
 $(window).on('load', function() {
   var chartIsotope = $('.product-container').isotope({
-    itemSelector: '.product-item',
-    layoutMode: 'fitRows'
+    itemSelector: '.product-item'
   });
 
   $('#product-filters li').on('click', function() {
@@ -143,5 +139,4 @@ $(window).on('load', function() {
     });
   //   aos_init();
   });
-
 });
