8101d06255c89b134a097945d9da2fa7b44396dc
Lei Chen <leichen88@yahoo.fr>
2024-07-01 14:18:10 +0200

    implement changes for text

--- app/products/gotm/2024-06/config.js
+++ app/products/gotm/2024-06/config.js
@@ -55,7 +55,7 @@ var config1 = {
             hidden: false,
             title: '',
             image: '',
-            description: '<p>Most reservations are made to the rights of refugees to (self-)employment (Articles 17, 18 and 19) and social protection (Article 24), which together provide for the right of refugees to decent work.</p><figure class="figure mt-4 mb-0"><img src="RF1336924_202311211209_DSCF2506_jg.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Mexico. Makendy and Lourdes fled Haiti due to a dire humanitarian situation caused by gang violence, insecurity, and lack of access to basic services.<br>©UNHCR/Jeoffrey Guillemard</figcaption></figure>',
+            description: '<p>Most reservations are made to the rights of refugees to (self-)employment (Articles 17, 18 and 19) and social protection (Article 24), which together provide for the right of refugees to decent work.</p><figure class="figure mt-4 mb-0"><img src="RF1336924_202311211209_DSCF2506_jg.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Mexico. Makendy and Lourdes fled Haiti due to a dire humanitarian situation caused by gang violence, insecurity, and lack of access to basic services. As part of the Local Integration Programme (PIL) they were relocated from Chiapas to Aguascalientes, where they found a permanent job at Exedy Dynax, a Japanese company that manufactures automotive parts.<br>©UNHCR/Jeoffrey Guillemard</figcaption></figure>',
             location: {
                 center: [20, 20],
                 zoom: 1.7,
@@ -91,7 +91,7 @@ var config1 = {
             hidden: false,
             title: '',
             image: '',
-            description: '<p>Some States do not recognize the freedom of movement of refugees and maintain reservations to Article 26. The inability of refugees to freely move around their country of asylum may prevent them from accessing education or employment opportunities.</p><figure class="figure mt-4 mb-0"><img src="RF2263530__MG_8015.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Maya Ghazal, 20, pictured after her first solo flight, at The Pilot Centre, in Denham, United Kingdom.<br>©UNHCR/Andrew McConnell</figcaption></figure>',
+            description: '<p>Some States decide to limit the freedom of movement of refugees or their right to freely choose their place of residence and maintain reservations to Article 26. Such restrictions may prevent refugees from exercising other rights, such as accessing education or employment opportunities.</p><figure class="figure mt-4 mb-0"><img src="RF2263530__MG_8015.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Maya Ghazal, 20, pictured after her first solo flight, at The Pilot Centre, in Denham, United Kingdom. She dreams of becoming the first female Syrian refugee pilot and flying for a commercial airline.<br>©UNHCR/Andrew McConnell</figcaption></figure>',
             location: {
                 center: [20, 20],
                 zoom: 1.7,
@@ -164,7 +164,7 @@ var config1 = {
             hidden: false,
             title: '',
             image: '',
-            description: '<p>Some States are also hesitant to provide refugees with their full rights related to public education and maintain reservations to Article 22. This may include limitations to access secondary education, the recognition of certificates, and opportunities for refugees to finance their studies. This may affect refugees’ ability to build their future and lead independent lives.</p><figure class="figure mt-4 mb-0"><img src="RF1265680_20220913_DAFI-MEX_TARDY_0086.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Roxana, 23 years old, from Maracaibo in northwestern Venezuela, is a final-year BA student of Accounting at the University of Guanajuato, in Mexico, on a DAFI scholarship.<br>©UNHCR/Antoine Tardy</figcaption></figure>',
+            description: '<p>Some States are also hesitant to provide refugees with their full rights related to public education and maintain reservations to Article 22. This may include limitations to accessing secondary and higher education, the recognition of certificates, and opportunities for refugees to finance their studies. This may affect refugees’ ability to build their future and lead independent lives.</p><figure class="figure mt-4 mb-0"><img src="RF1265680_20220913_DAFI-MEX_TARDY_0086.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Roxana, 23 years old, from Maracaibo in northwestern Venezuela, is a final-year BA student of Accounting at the University of Guanajuato, in Mexico, on a DAFI scholarship. The registration process was very stressful for her because she did not know whether she would be allowed to study.<br>©UNHCR/Antoine Tardy</figcaption></figure>',
             location: {
                 center: [20, 20],
                 zoom: 1.7,
@@ -200,7 +200,7 @@ var config1 = {
             hidden: false,
             title: '',
             image: '',
-            description: 'Positively, 17 States have withdrawn some or all their reservations, granting refugees more rights.',
+            description: 'Positively, 17 States have withdrawn some or all their reservations, giving refugees access to more rights.',
             location: {
                 center: [20, 20],
                 zoom: 1.7,
@@ -332,7 +332,7 @@ var config2 = {
             hidden: false,
             title: '',
             image: '',
-            description: 'In 2023, UNHCR has recorded at least 92 legislative developments in 77 countries, 49 of which represented advances in refugee rights.',
+            description: 'In 2023, UNHCR has recorded at least 92 legislative changes relating to the protection of refugees and asylum-seekers in 77 countries and territories.',
             location: {
                 center: [20, 20],
                 zoom: 1.7,
@@ -431,7 +431,7 @@ var config2 = {
             hidden: false,
             title: '',
             image: '',
-            description: 'Also in 2023, Chad signed the implementing decree of its 2020 Asylum Law, which was a result of the country’s commitments made during the first Global Refugee Forum (GRF) in 2019. The law is expected to strengthen the framework for the protection of refugees and asylum-seekers and to ensure the respect for their civil and socio-economic rights.<figure class="figure mt-4 mb-0"><img src="RF1219449_Colin_delfosse_09_04_2022-11.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Refugee teacher Hassanie Ahmad Hussein, 28, teaches a class outdoors at the school in Kouchaguine-Moura refugee camp in eastern Chad and dreams of a chance to continue her studies with a masters and doctorate.<br>©UNHCR/Colin Delfosse</figcaption></figure>',
+            description: 'Also in 2023, Chad signed the implementing decree of its 2020 Asylum Law, which was a result of the country’s commitments made during the first Global Refugee Forum (GRF) in 2019. The law is expected to strengthen the framework for the protection of refugees and asylum-seekers and to ensure the respect for their civil and socio-economic rights.<figure class="figure mt-4 mb-0"><img src="RF1219449_Colin_delfosse_09_04_2022-11.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">Refugee teacher Hassanie Ahmad Hussein, 28, teaches a class outdoors at the school in Kouchaguine-Moura refugee camp in eastern Chad. She dreams of continuing her studies with a master’s degree and doctorate.<br>©UNHCR/Colin Delfosse</figcaption></figure>',
             location: {
                 center: [18.40685, 15.17010],
                 zoom: 4.5,
@@ -498,7 +498,7 @@ var config2 = {
             hidden: false,
             title: '',
             image: '',
-            description: 'In 2021, Colombia enacted a decree providing Temporary Protection Status (TPS) for an estimated 1.8 million Venezuelans. The creation of the TPS was a milestone in the region because it allows eligible Venezuelans to regularize their stay and reside in Colombia for ten years, which includes accessing a full range of rights.<figure class="figure mt-4 mb-0"><img src="RF1217449_Foto_08_Entregas_Masivas_PPTBogota_26012022_Daniela_Camargo.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">On February 2022, Andrés from Venezuela received his Temporary Protection Permit in Bogotá.<br>©UNHCR/Daniela Camargo</figcaption></figure>',
+            description: 'In 2021, Colombia enacted a decree providing Temporary Protection Status (TPS) for an estimated 1.8 million Venezuelans. The creation of the TPS was a milestone in the region because it allows eligible Venezuelans to regularize their stay and reside in Colombia for ten years, which includes accessing a full range of rights.<figure class="figure mt-4 mb-0"><img src="RF1217449_Foto_08_Entregas_Masivas_PPTBogota_26012022_Daniela_Camargo.jpg" class="figure-img img-fluid" alt=""><figcaption class="figure-caption">On February 2022, Andrés from Venezuela received his Temporary Protection Permit in Bogotá. He hopes that this will enable him to register his motorcycle for his work, open a bank account and certify and grow his business.<br>©UNHCR/Daniela Camargo</figcaption></figure>',
             location: {
                 center: [-73.89097, 4.34697],
                 zoom: 4.5,
